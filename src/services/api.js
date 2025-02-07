af044e880d97bf9d18bd1a1e64e02831301c3905
kasim <kalagotlakasimpeera@gmail.com>
2025-02-07 11:38:28 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-const API_URL = 'https://email-backend-4l8f.onrender.com/api'; // Adjust based on your backend
+const API_URL = 'https://email-backend-h319.onrender.com/api'; // Adjust based on your backend
 
 export const registerUser = (userData) => axios.post(`${API_URL}/register`, userData);
 export const loginUser = (loginData) => axios.post(`${API_URL}/login`, loginData);
 export const fetchEmails = (folder, email, emailPassword) =>
-  axios.get(`${API_URL}/emails/${folder}`, { params: { email, emailPassword } });
+  axios.get(`${API_URL}/${folder}`, { params: { email, emailPassword } });
+
+export const deleteEmail = async (emailId) => {
+  return axios.post(`${API_URL}/delete-email`, { emailId });
+};
